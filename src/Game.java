8b5abbd427bbeed78514bcc319aa2f3253c68bf6
Madzhun Khakim <hakimmadgun@gmail.com>
2022-12-18 02:39:20 +0600

    changed choice method add exception

--- src/Game.java
+++ src/Game.java
@@ -3,18 +3,19 @@ import model.Product;
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Game {
     public  void run(){
+        print("Вы торговец на просторах земель Скайрима.\n"+"Вам нужно закупиться товаром для продажи");
        List<Product> products = buyProduct();
     }
     public static List<Product> buyProduct(){
         List<Product> products = new ArrayList<>();
         Merchant merchant = new Merchant();
         merchant.setCarrying(80);
-        print("Вы торговец на просторах земель Скайрима.\n"+"Вам нужно закупиться товаром для продажи");
         int carrying = merchant.getCarrying();
         merchant.merchantInitialMoney();
         int money = merchant.getMoney();
@@ -23,41 +24,44 @@ public class Game {
         int i = 0;
         while (choice !=2){
             Product product = new Product();
-            choice = choice("Вы хотите купить товар? Да(1) Нет (2)");
-               if(choice ==1 && money>15 && carrying>14){
-                   products.add(product);
-                   product.getTypeofProduct();
-                   System.out.printf("Вы купили %s, потрачено (%s золота)",products.get(i).getType(),products.get(i).getPrice());
-               }else if (choice == 2){
-                   print("Покупка завершена");
-                   break;
-               } else if (money<15) {
-                   print("У Вас недостаточно золота для совершения покупки");
-                   break;
-               }else if (carrying<14){
-                   print("Не хватает места для совершения покупки, Вы будете перегружены");
-                   break;
-               }
-               money-=products.get(i).getPrice();
-               carrying-=products.get(i).getWeight();
-               System.out.printf("Ваше золото %s, Ваша грузоподъемность %s%n",money,carrying);
-               i++;
+                choice = choice("Вы хотите купить товар? Да(1) Нет (2)");
+
+                if(choice ==1 && money>15 && carrying>14){
+                    products.add(product);
+                    product.getTypeofProduct();
+                    System.out.printf("Вы купили %s, потрачено (%s золота)",products.get(i).getType(),products.get(i).getPrice());
+                    money-=products.get(i).getPrice();
+                    carrying-=products.get(i).getWeight();
+                    System.out.printf("Ваше золото %s, Ваша грузоподъемность %s%n",money,carrying);
+                    i++;
+                }else if (choice == 2 && carrying!=80){
+                    print("Покупка завершена");
+                    break;
+                } else if (money<15) {
+                    print("У Вас недостаточно золота для совершения покупки");
+                    break;
+                }else if (carrying<14){
+                    print("Не хватает места для совершения покупки, Вы будете перегружены");
+                    break;
+                }
+
+
         }
         return products;
     }
     public static void print(String message){
         System.out.println(message);
     }
-
-    public static int choice(String message){
+    public static int choice(String message) {
         print(message);
-        Scanner sc = new Scanner(System.in);
-        try {
-            int choice = sc.nextInt();
-            return choice;
-        }catch (Exception e){
-            e.printStackTrace();
-            return choice("Данное действие недоступно");
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            try {
+                return input.nextInt();
+            }
+            catch (java.util.InputMismatchException e) {
+                return choice("Действие недоступно, выберите действие повторно\n");
+            }
         }
     }
 }
